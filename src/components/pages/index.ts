152523a533b06648959c2d4a106634ab5a55cea3
bodrozicN <nikolabodrozic1010@gmail.com>
2022-05-16 14:29:15 +0200

    added pages folder with basic boilerplate

---
+++ src/components/pages/index.ts
@@ -0,0 +1,4 @@
+export { default as Home } from "./home";
+export { default as Coin } from "./coin";
+export { default as Error } from "./error";
+export { default as Portfolio } from "./portfolio";
