191588203bd6365ef23590917d5d69e026c53f67
bodrozicN <nikolabodrozic1010@gmail.com>
2022-05-16 14:30:34 +0200

    created routes for diffrent pages and implemented lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
+import { BrowserRouter as Router, Link, Route, Routes } from "react-router-dom";
+
+const Home = lazy(() => import("./components/pages/home"));
+const Coin = lazy(() => import("./components/pages/coin"));
+const Portfolio = lazy(() => import("./components/pages/portfolio"));
+const Error = lazy(() => import("./components/pages/error"));
 
 function App() {
-  return <div></div>;
+  return (
+    <>
+      <Suspense fallback={<p>Loading...</p>}>
+        <Router>
+          <Link to="/portfolio">aj</Link>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/coin/:id" element={<Coin />} />
+            <Route path="/portfolio" element={<Portfolio />} />
+            <Route path="*" element={<Error />} />
+          </Routes>
+        </Router>
+      </Suspense>
+    </>
+  );
 }
 
 export default App;
