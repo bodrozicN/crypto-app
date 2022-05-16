0178dc2bcd12ab037bb4c484776a6c4676da1742
bodrozicN <nikolabodrozic1010@gmail.com>
2022-05-16 15:27:54 +0200

    implemented global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React, { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "./style";
 
 const Home = lazy(() => import("./components/pages/home"));
 const Coin = lazy(() => import("./components/pages/coin"));
@@ -9,16 +11,19 @@ const Error = lazy(() => import("./components/pages/error"));
 function App() {
   return (
     <>
-      <Suspense fallback={<p>Loading...</p>}>
-        <Router>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/coin/:id" element={<Coin />} />
-            <Route path="/portfolio" element={<Portfolio />} />
-            <Route path="*" element={<Error />} />
-          </Routes>
-        </Router>
-      </Suspense>
+      <GlobalStyle />
+      <ThemeProvider theme={{ isDark: false }}>
+        <Suspense fallback={<p>Loading...</p>}>
+          <Router>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/coin/:id" element={<Coin />} />
+              <Route path="/portfolio" element={<Portfolio />} />
+              <Route path="*" element={<Error />} />
+            </Routes>
+          </Router>
+        </Suspense>
+      </ThemeProvider>
     </>
   );
 }
