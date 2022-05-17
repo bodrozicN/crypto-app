6517d1d06d6ce6a0190db4bec08433cf53ae45d3
bodrozicN <nikolabodrozic1010@gmail.com>
2022-05-17 14:10:19 +0200

    created span component

---
+++ src/components/atoms/span/style.ts
@@ -0,0 +1,34 @@
+import styled, { css } from "styled-components";
+import { OFontWeight } from "../../../types";
+
+const LabelSpan = css`
+  font-size: 1.4rem;
+  font-weight: ${OFontWeight.light};
+  font-style: normal;
+`;
+
+export const HeroLabel = styled.span`
+  ${LabelSpan}
+  color: var(--white);
+`;
+
+export const FilterLabel = styled.span`
+  ${LabelSpan}
+  color: var(--grey-light-2);
+`;
+
+const HeroSpanValue = css`
+  font-size: 1.4rem;
+  font-weight: ${OFontWeight.bold};
+  font-style: normal;
+`;
+
+export const HeroValue = styled.span`
+  ${HeroSpanValue}
+  color: var(--white);
+`;
+
+export const HeroChar = styled.span`
+  ${HeroSpanValue}
+  color: var(--grey-light-1);
+`;
