c649ee92cf99642a99db0d60cd782891cb9d63fc
bodrozicN <nikolabodrozic1010@gmail.com>
2022-05-18 16:58:43 +0200

    code refactoring for filter component

--- src/components/moleculs/filter/filter.test.tsx
+++ src/components/moleculs/filter/filter.test.tsx
@@ -3,22 +3,15 @@ import Filter from ".";
 
 describe("Filter", () => {
   it("Should appear on screen", () => {
-    render(<Filter type="heroFilter" />);
+    const onChange = jest.fn();
+    render(<Filter onChange={onChange} type="currencyFilter" />);
     const select = screen.getByRole("filter");
     expect(select).toBeInTheDocument();
   });
 
-  it("Should have correct style", () => {
-    render(<Filter type="heroFilter" />);
-    const select = screen.getByRole("filter");
-    expect(select).toHaveStyle(`
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;`);
-  });
-
   it("Should have children", () => {
-    render(<Filter type="heroFilter" />);
+    const onChange = jest.fn();
+    render(<Filter onChange={onChange} type="currencyFilter" />);
     const span = screen.getByRole("span");
     expect(span).toBeInTheDocument();
     const select = screen.getByRole("select");
