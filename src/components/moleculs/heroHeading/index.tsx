1b6e92345d398bcac0df4663d3168d8d45f01393
bodrozicN <nikolabodrozic1010@gmail.com>
2022-05-21 13:35:34 +0200

    refactored heroHeading component

--- src/components/moleculs/heroHeading/index.tsx
+++ src/components/moleculs/heroHeading/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Heading } from "../../atoms";
 import { StyledHeadingHero } from "./style";
-import { OFontWeight } from "../../../types";
 
 type Props = {
   value1: string | number;
@@ -11,8 +10,8 @@ type Props = {
 const HeroHeading = ({ value1, value2 }: Props) => {
   return (
     <StyledHeadingHero role="heroheading">
-      <Heading title={value1} type="h1" weight={OFontWeight.bold} />
-      <Heading title={value2} type="h1" weight={OFontWeight.light} />
+      <Heading title={value1} type="h1" $isBold />
+      <Heading title={value2} type="h1" />
     </StyledHeadingHero>
   );
 };
