d347bf331e02875a151cb21ddec634b940bfa7c3
bodrozicN <nikolabodrozic1010@gmail.com>
2022-05-21 13:28:01 +0200

    filter component refactored

--- src/components/moleculs/filter/filter.test.tsx
+++ src/components/moleculs/filter/filter.test.tsx
@@ -1,20 +1,17 @@
-import { screen, render } from "@testing-library/react";
 import Filter from ".";
+import { screen, render } from "@testing-library/react";
+import { OFIlter } from "../../../types";
 
 describe("Filter", () => {
-  it("Should appear on screen", () => {
-    const onChange = jest.fn();
-    render(<Filter onChange={onChange} type="currencyFilter" />);
-    const select = screen.getByRole("filter");
-    expect(select).toBeInTheDocument();
+  const onChange = jest.fn();
+  it("renders without crashing", () => {
+    render(<Filter onChange={onChange} type={OFIlter.currency} />);
+    expect(screen.getByRole("filter")).toBeInTheDocument();
   });
 
-  it("Should have children", () => {
-    const onChange = jest.fn();
-    render(<Filter onChange={onChange} type="currencyFilter" />);
-    const span = screen.getByRole("span");
-    expect(span).toBeInTheDocument();
-    const select = screen.getByRole("select");
-    expect(select).toBeInTheDocument();
+  it("renders the correct elements", () => {
+    render(<Filter onChange={onChange} type={OFIlter.currency} />);
+    expect(screen.getByText("Currency")).toBeInTheDocument();
+    expect(screen.getByRole("listbox")).toBeInTheDocument();
   });
 });
--- src/components/moleculs/filter/index.tsx
+++ src/components/moleculs/filter/index.tsx
@@ -1,57 +1,24 @@
 import React from "react";
-import { Select, Span } from "../../atoms";
+import { Select, Span, OSpan } from "../../atoms";
 import { StyledFilter } from "./style";
+import { TFilter, OFIlter } from "../../../types";
 
 type Props = {
   onChange: (e: React.ChangeEvent<HTMLSelectElement>) => void;
-  type:
-    | "currencyFilter"
-    | "orderDirFilter"
-    | "orderByFilter"
-    | "limitFilter"
-    | "timePeriodFilter";
+  type: TFilter;
 };
 
 const Filter = (props: Props) => {
-  switch (props.type) {
-    case "currencyFilter":
-      return (
-        <StyledFilter role="filter">
-          <Span type="heroLabel" title="Currency" />
-          <Select type="currencySelect" onChange={props.onChange} />
-        </StyledFilter>
-      );
-    case "limitFilter":
-      return (
-        <StyledFilter role="filter">
-          <Span type="filterLabel" title="Rows" />
-          <Select type="limitSelect" onChange={props.onChange} />
-        </StyledFilter>
-      );
-    case "orderByFilter":
-      return (
-        <StyledFilter role="filter">
-          <Span type="filterLabel" title="Sort by" />
-          <Select type="orderBySelect" onChange={props.onChange} />
-        </StyledFilter>
-      );
-    case "orderDirFilter":
-      return (
-        <StyledFilter role="filter">
-          <Span type="filterLabel" title="Order" />
-          <Select type="orderDirectionSelect" onChange={props.onChange} />
-        </StyledFilter>
-      );
-    case "timePeriodFilter":
-      return (
-        <StyledFilter role="filter">
-          <Span type="filterLabel" title="Period" />
-          <Select type="timePeriodSelect" onChange={props.onChange} />
-        </StyledFilter>
-      );
-    default:
-      throw new Error("Unknown filter type");
-  }
+  // if it is 'currency' type, then we need to show the heroPrimary span element, otherwise, we need to show the filter span element
+  const spanType =
+    props.type === OFIlter.currency ? OSpan.heroPrimary : OSpan.filterLabel;
+
+  return (
+    <StyledFilter role="filter">
+      <Span type={spanType} content={props.type} />
+      <Select type={props.type} onChange={props.onChange} />
+    </StyledFilter>
+  );
 };
 
 export default Filter;
