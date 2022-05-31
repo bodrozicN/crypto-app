42d41678d9cdded51bcfc73ec1d5a42d9afc9d5e
bodrozicN <nikolabodrozic1010@gmail.com>
2022-05-31 21:49:45 +0200

    coin img filter removed

--- src/components/atoms/img/style.ts
+++ src/components/atoms/img/style.ts
@@ -9,9 +9,4 @@ export const StyledImg = styled.img<IStyledImg>`
   border-radius: 50%;
   height: 4rem;
   width: 4rem;
-
-  filter: ${({ theme }) =>
-    theme.isDark
-      ? "sepia(200%) hue-rotate(200deg) saturate(0%)"
-      : "sepia(200%) hue-rotate(200deg) saturate(3000%)"};
 `;
