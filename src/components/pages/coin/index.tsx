1ea2895a9572028e9c0009302fe4fcabc4cae77c
bodrozicN <nikolabodrozic1010@gmail.com>
2022-06-05 11:01:47 +0200

    coin page component refactored

--- src/components/pages/coin/index.tsx
+++ src/components/pages/coin/index.tsx
@@ -1,94 +1,18 @@
-import React, { useState } from "react";
-import { useParams } from "react-router-dom";
-import {
-  coinPageChartDataFormatter,
-  chartFilters,
-  excangesTableHead,
-} from "../../../helpers";
-import { useFilters } from "../../../hooks";
-import {
-  useGetCoinPriceHistoryQuery,
-  useGetCoinQuery,
-  useGetCoinOhlcQuery,
-  useGetCoinExcangesQuery,
-} from "../../../redux/api";
-import { OTimePeriod, TTimePeriod } from "../../../types";
 import { CoinHero, Header, ChartSection, CoinInfo } from "../../organisms";
 import { Wrapper } from "./style";
+import { useCoinLogic } from "./useCoinLogic";
 
 const Coin = () => {
-  const { currency, handleSelect } = useFilters();
-  const { uuid } = useParams();
-  const [timePeriod, setTimePeriod] = useState<TTimePeriod>(OTimePeriod.daily);
-
-  const { coinDetails } = useGetCoinQuery(
-    { currency, uuid },
-    {
-      selectFromResult: ({ data }) => ({
-        coinDetails: data?.data.coin,
-      }),
-    }
-  );
-
-  const { priceHistory } = useGetCoinPriceHistoryQuery(
-    {
-      currency,
-      timePeriod,
-      uuid,
-    },
-    {
-      selectFromResult: ({ data }) => ({
-        priceHistory: coinPageChartDataFormatter(data?.data.history),
-      }),
-    }
-  );
-
-  const { coinOhlc } = useGetCoinOhlcQuery(
-    {
-      currency,
-      uuid,
-    },
-    {
-      selectFromResult: ({ data }) => ({
-        coinOhlc: data?.data.ohlc,
-      }),
-    }
-  );
-
-  const { excanges } = useGetCoinExcangesQuery(
-    {
-      currency,
-      uuid,
-    },
-    {
-      selectFromResult: ({ data }) => ({
-        excanges: data?.data.exchanges,
-      }),
-    }
-  );
-
-  const ohlc = {
-    low: coinOhlc && coinOhlc[0]?.low,
-    high: coinOhlc && coinOhlc[0]?.high,
-    close: coinOhlc && coinOhlc[0]?.close,
-  };
-
-  const chartSectionProps = {
-    timePeriod,
-    setTimePeriod,
-    name: coinDetails?.name,
-    priceHistory,
-    chartFilters,
-  };
+  const { chartSectionProps, coinHeroProps, coinInfoProps } = useCoinLogic();
 
   return (
     <>
       <Wrapper>
         <Header />
-        <CoinHero {...{ coinDetails, handleSelect, currency, ohlc }} />
+        <CoinHero {...coinHeroProps} />
       </Wrapper>
       <ChartSection {...chartSectionProps} />
-      <CoinInfo theadContent={excangesTableHead} excanges={excanges} />
+      <CoinInfo {...coinInfoProps} />
     </>
   );
 };
