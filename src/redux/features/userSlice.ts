0be4f8132a6a0db8418529941017df071f5479ca
Nikola Bodrozic <bodrozicnikola3@gmail.com>
2024-02-29 20:19:54 +0100

    BUGFIX: Enable ui to render

--- src/redux/features/userSlice.ts
+++ src/redux/features/userSlice.ts
@@ -20,67 +20,63 @@ const userSlice = createSlice({
   initialState,
   reducers: {},
   extraReducers: (builder) => {
-    builder.addCase(createUserAccount.pending, (state) => {
-      state.isLoading = true;
-      state.isSuccess = false;
-      state.isError = false;
-    });
-    builder.addCase(
-      createUserAccount.fulfilled,
-      (state, { payload }: PayloadAction<User>) => {
-        state.user = payload;
-        state.isSuccess = true;
-        state.isError = false;
-        state.isLoading = false;
-        localStorage.setItem("user", JSON.stringify(payload));
-      }
-    );
-    builder.addCase(createUserAccount.rejected, (state) => {
-      state.isError = true;
-      state.isLoading = false;
-      state.isSuccess = false;
-    });
-
-    builder.addCase(logOut.pending, (state) => {
-      state.isError = false;
-      state.isLoading = true;
-      state.isSuccess = false;
-    });
-
-    builder.addCase(logOut.fulfilled, (state) => {
-      state.user = null;
-      state.isError = false;
-      state.isLoading = false;
-      state.isSuccess = true;
-      localStorage.setItem("user", JSON.stringify(null));
-    });
-
-    builder.addCase(logOut.rejected, (state) => {
-      state.isError = true;
-      state.isLoading = false;
-      state.isSuccess = false;
-    });
-
-    builder.addCase(logIn.pending, (state) => {
-      state.isError = false;
-      state.isLoading = true;
-      state.isSuccess = false;
-    });
-    builder.addCase(
-      logIn.fulfilled,
-      (state, { payload }: PayloadAction<User>) => {
-        state.user = payload;
-        state.isError = false;
-        state.isLoading = false;
-        state.isSuccess = true;
-        localStorage.setItem("user", JSON.stringify(payload));
-      }
-    );
-    builder.addCase(logIn.rejected, (state) => {
-      state.isError = true;
-      state.isLoading = false;
-      state.isSuccess = false;
-    });
+    // builder.addCase(createUserAccount.pending, (state) => {
+    //   state.isLoading = true;
+    //   state.isSuccess = false;
+    //   state.isError = false;
+    // });
+    // builder.addCase(
+    //   createUserAccount.fulfilled,
+    //   (state, { payload }: PayloadAction<User>) => {
+    //     state.user = payload;
+    //     state.isSuccess = true;
+    //     state.isError = false;
+    //     state.isLoading = false;
+    //     localStorage.setItem("user", JSON.stringify(payload));
+    //   }
+    // );
+    // builder.addCase(createUserAccount.rejected, (state) => {
+    //   state.isError = true;
+    //   state.isLoading = false;
+    //   state.isSuccess = false;
+    // });
+    // builder.addCase(logOut.pending, (state) => {
+    //   state.isError = false;
+    //   state.isLoading = true;
+    //   state.isSuccess = false;
+    // });
+    // builder.addCase(logOut.fulfilled, (state) => {
+    //   state.user = null;
+    //   state.isError = false;
+    //   state.isLoading = false;
+    //   state.isSuccess = true;
+    //   localStorage.setItem("user", JSON.stringify(null));
+    // });
+    // builder.addCase(logOut.rejected, (state) => {
+    //   state.isError = true;
+    //   state.isLoading = false;
+    //   state.isSuccess = false;
+    // });
+    // builder.addCase(logIn.pending, (state) => {
+    //   state.isError = false;
+    //   state.isLoading = true;
+    //   state.isSuccess = false;
+    // });
+    // builder.addCase(
+    //   logIn.fulfilled,
+    //   (state, { payload }: PayloadAction<User>) => {
+    //     state.user = payload;
+    //     state.isError = false;
+    //     state.isLoading = false;
+    //     state.isSuccess = true;
+    //     localStorage.setItem("user", JSON.stringify(payload));
+    //   }
+    // );
+    // builder.addCase(logIn.rejected, (state) => {
+    //   state.isError = true;
+    //   state.isLoading = false;
+    //   state.isSuccess = false;
+    // });
   },
 });
 
