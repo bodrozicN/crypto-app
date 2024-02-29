0092ca55ab9f9632cf49e4fb25f44c692cc489ed
Nikola Bodrozic <bodrozicnikola3@gmail.com>
2024-02-29 21:52:54 +0100

    TASK: Implement import aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig(() => {
   return {
@@ -7,5 +8,10 @@ export default defineConfig(() => {
       outDir: "build",
     },
     plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src/"),
+      },
+    },
   };
 });
